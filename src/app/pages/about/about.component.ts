9c9e71b2083ab9023cccbc3b0caddcc2f8d049d2
Prottoy Ghosh <ghoshprottoy98@gmail.com>
2023-02-20 12:23:29 +0600

    6th commit

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AboutComponent {
 
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 }
--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 
 }
